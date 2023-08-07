f64c8d64ce2b47d15752ea6ae14a97c3b5840f40
Mohamed Shaaban <moshaaban500@gmail.com>
2023-08-07 09:23:13 +0300

    Add project files.

---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+        }
+
+        private static Singleton _instance;
+
+
+        private static readonly object _lock = new object();
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
