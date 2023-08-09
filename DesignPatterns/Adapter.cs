fb71a78a6cbc4ca122920d86942160768154c9aa
Mohamed Shaaban <moshaaban500@gmail.com>
2023-08-09 10:22:19 +0300

    Adding Adapter Design Pattern

---
+++ DesignPatterns/Adapter.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns
+{
+    public interface IClientTarget
+    {
+        string GetRequest();
+    }
+
+    public class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request";
+        }
+    }
+
+    public class Adapter : IClientTarget
+    {
+        private readonly Adaptee _adaptee;
+        public Adapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return _adaptee.GetSpecificRequest();
+        }
+    }
+}
