985416a535f3fcd5d197d59cd3eb20b07a0eae92
Mohamed Shaaban <moshaaban500@gmail.com>
2023-08-10 03:35:01 +0300

    Adding Strategy Design Pattern

---
+++ DesignPatterns/Strategy.cs
@@ -0,0 +1,52 @@
+﻿namespace DesignPatterns
+{
+    public interface IStrategy
+    {
+        void Execute();
+    }
+
+    public class StrategyA : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing strategy A");
+        }
+    }
+
+    public class StrategyB : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Executing strategy B");
+        }
+    }
+
+    public class StrategyContext
+    {
+        private IStrategy _strategy;
+        public void SetStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+        public void ExecuteStrategy()
+        {
+            _strategy.Execute();
+        }
+    }
+
+}
+
+/*
+ 
+// in the main program
+var context = new StrategyContext();
+
+// Use the strategy A
+context.SetStrategy(new ConcreteStrategyA());
+context.ExecuteStrategy();
+
+// Use the strategy B
+context.SetStrategy(new ConcreteStrategyB());
+context.ExecuteStrategy();
+
+*/
