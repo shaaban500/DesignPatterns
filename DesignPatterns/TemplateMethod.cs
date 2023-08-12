18673110afb8114bf42567133ccccadcd80247c5
Mohamed Shaaban <moshaaban500@gmail.com>
2023-08-12 07:10:54 +0300

    adding Template Method design pattern

---
+++ DesignPatterns/TemplateMethod.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns
+{
+    public abstract class AbstractClass
+    {
+        protected void Operation1()
+        {
+            Console.WriteLine("Operation1");
+        }
+
+        protected abstract void Operations2();
+
+        protected void Operation3()
+        {
+            Console.WriteLine("Operation3");
+        }
+    }
+
+    public class ConcreteClass1 : AbstractClass
+    {
+        protected override void Operations2()
+        {
+            Console.WriteLine("Implemented Operation2");
+        }
+    }
+
+    public class ConcreteClass2 : AbstractClass
+    {
+        protected override void Operations2()
+        {
+            Console.WriteLine("Implemented Operation2");
+        }
+    }
+}
+
